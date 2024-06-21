8c8c106efa3b12a8855f96a1cea05d3c9ff007ff
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-06-21 11:43:55 +0500

    first

---
+++ Tanishuv darsi/Homework_1.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Ismingiz : ";
+$name = fgets(STDIN);
+$name = trim($name);
+
+echo "Familiyangiz : ";
+$surname = fgets(STDIN);
+$surname = trim($surname);
+
+echo "Yoshingiz : ";
+$age = fgets(STDIN);
+$age = trim($age);
+
+echo "Tug'ulgan yilinigiz : ";
+$year = fgets(STDIN);
+$year = trim($year);
+
+echo "\nAssalomu aleykum, Mening ismim $name, familiyam $surname.Men $age yoshdaman. Men $year da tavallud topganman!\n";
+
+?>
