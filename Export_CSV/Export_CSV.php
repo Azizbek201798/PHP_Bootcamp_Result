c2b5ba596e708ca7eafcc00f48512d2b7aecaae9
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-07-05 01:13:16 +0500

    Star

--- Export_CSV/Export_CSV.php
+++ Export_CSV/Export_CSV.php
@@ -257,7 +257,7 @@ if (isset($_GET['daily_id'])) {
                     <div class="modal-dialog">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Are you Sure?</h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
