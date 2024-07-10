62b3596b31d518eb2f0907bc02f660df47fc2992
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-07-10 15:45:41 +0500

    HW

---
+++ Tg_bot_Convertor/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    declare(strict_types=1);
+    require 'vendor/autoload.php';
+    
+    use GuzzleHttp\Client;
+    
+    $token = "7284381414:AAHv-6aSoVU2c98Z3MQug0WuEotG_qPt75o";
+    $tgApi = "https://api.telegram.org/bot$token/";
+    
+    $client = new Client([
+        'base_uri' => $tgApi,
+    ]);
+    
+    $responce = $client->post('sendMessage',[
+        'form_params' => [
+            'chat_id' => '5646244166',
+            'text' => 'Akalariga ukalaridayman!',
+        ]
+    ]);
+    
+    $json = $responce->getBody()->getContents();
+    print_r(json_decode($json,true));
+
+?>
