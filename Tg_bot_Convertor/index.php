c39f65eb4844d8b5580a88618cdbf025d702dd91
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-07-11 13:43:25 +0500

    HW

--- Tg_bot_Convertor/index.php
+++ Tg_bot_Convertor/index.php
@@ -1,49 +1,52 @@
 <?php
 
-    declare(strict_types=1);
+declare(strict_types=1);
 
-    require 'vendor/autoload.php';
-    
-    use GuzzleHttp\Client;
-    
-    $token = "7284381414:AAHv-6aSoVU2c98Z3MQug0WuEotG_qPt75o";
-    $tgApi = "https://api.telegram.org/bot$token/";
-    
-    $client = new Client(['base_uri' => $tgApi,]);
-    $currency = new Client(['base_uri' => 'https://cbu.uz/oz/arkhiv-kursov-valyut/json/']);
+require 'vendor/autoload.php';
 
-    $update = json_decode(file_get_contents('php://input'),true);
+use GuzzleHttp\Client;
 
-    if (isset($update)){
-        if(isset($update->message)){
+$token = "7284381414:AAHv-6aSoVU2c98Z3MQug0WuEotG_qPt75o";
+$tgApi = "https://api.telegram.org/bot$token/";
 
-            $message = $update->message;
-            $chat_id = $message->chat_id;
-            $miid =$message->message_id;
-            $name = $message->from->first_name;
-            $fromid = $message->from->id;
-            $text = $message->text;
-            
-            $exp = explode('-',$text);
-            $data = json_decode($currency->get("")->getBody()->getContents(),true);
+$client = new Client(['base_uri' => $tgApi]);
+$currency = new Client(['base_uri' => 'https://cbu.uz/oz/arkhiv-kursov-valyut/json/']);
 
-            $data[""] = $fromid;
-            foreach($data as $current){                
-                if($current['Ccy'] == $exp[1]){
-                    $text = round( ((int)$exp[0] / (int)$current[0]),2);
-                    break;
-                }
+$update = json_decode(file_get_contents('php://input'), true);
+
+if (isset($update)) {
+    if (isset($update['message'])) {
+        $message = $update['message'];
+        $chat_id = "5646244166";
+        $message_id = $message['message_id'];
+        $text = $message['text'];
+
+        $exp = explode('-', $text);
+        $currencyCode = strtoupper($exp[1]);
+
+        $response = $currency->get('');
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        $convertedAmount = '';
+        foreach ($data as $current) {
+            if ($current['Ccy'] === $currencyCode && $current['Rate'] !== 0) {
+                $convertedAmount = round((float)$exp[0] / (float)$current['Rate'], 2);
+                break;
             }
+        }
 
-            $client->post('sendMessage', [
+        if ($convertedAmount !== '') {
+            $responseText = "1-$currencyCode = $convertedAmount-$exp[1]";
+        } else {
+            $responseText = "Sorry, the conversion rate for $currencyCode is not available.";
+        }
+
+        $client->post('sendMessage', [
             'form_params' => [
                 'chat_id' => $chat_id,
-                'text' => "data: " . json_encode($exp)
-            ]
-            ]);
-
-            
-            }
+                'text' => $responseText,
+            ],
+        ]);
     }
-
+}
 ?>
