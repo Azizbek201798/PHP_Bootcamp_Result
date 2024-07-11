e1efd503709eef5f69724bc987a1fc51ab144419
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-07-11 13:07:09 +0500

    HW

--- Tg_bot_Convertor/index.php
+++ Tg_bot_Convertor/index.php
@@ -1,6 +1,7 @@
 <?php
 
     declare(strict_types=1);
+
     require 'vendor/autoload.php';
     
     use GuzzleHttp\Client;
@@ -11,8 +12,7 @@
     $client = new Client(['base_uri' => $tgApi,]);
     $currency = new Client(['base_uri' => 'https://cbu.uz/oz/arkhiv-kursov-valyut/json/']);
 
-    $update = json_decode(file_get_contents('php://input'));
-    var_dump($update);
+    $update = json_decode(file_get_contents('php://input'),true);
 
     if (isset($update)){
         if(isset($update->message)){
@@ -25,7 +25,15 @@
             $text = $message->text;
             
             $exp = explode('-',$text);
-            
+            $data = json_decode($currency->get("")->getBody()->getContents(),true);
+
+            foreach($data as $current){                
+                if($current['Ccy'] == $exp[1]){
+                    $text = round( ((int)$exp[0] / (int)$current[0]),2);
+                    break;
+                }
+            }
+
             $client->post('sendMessage', [
             'form_params' => [
                 'chat_id' => $chat_id,
