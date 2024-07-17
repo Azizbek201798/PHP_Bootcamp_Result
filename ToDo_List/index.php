ae646d30448e5f8dd19929d79eac6cb7bce9fec1
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-07-17 18:10:28 +0500

    HW

---
+++ ToDo_List/index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>ToDo list</h1>
+    <h4>TODO app</h4>
+    <form action="">
+
+        <input type="checkbox" name="option1";>Create project folder
+        <input type="checkbox" name="option1";>Git init
+        <input type="checkbox" name="option1";>Create remote git repo
+
+    </form>
+</body>
+</html>
