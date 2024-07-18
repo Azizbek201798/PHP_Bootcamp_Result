77f7e6626b5905184e0fa901d83af36aba241018
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-07-18 15:26:33 +0500

    HW

--- ToDo_List/index.php
+++ ToDo_List/index.php
@@ -7,7 +7,7 @@ $pdo = DB::connect();
 
 $todo = new Todo($pdo);
 
-if(!empty($_POST) && $_POST['']){
+if(!empty($_POST)){
     if(strlen($_POST['text'])){
         $todo->setToDo($_POST['text']);
         header("Location: index.php");
