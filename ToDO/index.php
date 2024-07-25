1685e88126ce844ad4e04b88d067c0971815dad8
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-07-26 03:41:42 +0500

    Home

--- ToDO/index.php
+++ ToDO/index.php
@@ -3,6 +3,7 @@
     require 'vendor/autoload.php';
 
     $update = json_decode(file_get_contents('php://input'),true);
+
     if(isset($update->message)){
         if (isset($update->update_id)){
         require 'bot/bot.php';
@@ -12,4 +13,3 @@
 
     require 'bootstrap.php';
     require 'routes/api.php';
-    
